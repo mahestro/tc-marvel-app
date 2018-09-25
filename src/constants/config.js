10e6b5990febc14b4a3a3381ef197e62b404c091
mahestro <millanluiseduardo@gmail.com>
2018-09-25 21:34:32 +0200

    fix topcoder api security

--- src/constants/config.js
+++ src/constants/config.js
@@ -2,6 +2,6 @@
 const isProduction = true;
 
 export const TCMARVEL_API_BASE_URL = isProduction ? 'https://tc-marvel-api.herokuapp.com/api' : 'http://localhost:3001/api';
-export const TC_API_BASE_URL = 'http://api.topcoder.com';
+export const TC_API_BASE_URL = 'https://api.topcoder.com';
 export const MAIL_SENDER = 'mahestro.topcoder@gmail.com';
 export const MAIL_RECEIVER = 'mahestro.topcoder@gmail.com';
